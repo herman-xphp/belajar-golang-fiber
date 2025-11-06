316d4959a1fc3da704e1b06c60514b9f82861c51
herman-xphp <hermanxphp@gmail.com>
2025-11-06 20:09:27 +0800

    HTTP Response

--- fiber_test.go
+++ fiber_test.go
@@ -243,3 +243,22 @@ func TestBodyParserForm(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, "Register Success budi", string(bytes))
 }
+
+func TestResponseJSON(t *testing.T) {
+	app.Get("/user", func(ctx *fiber.Ctx) error {
+		return ctx.JSON(fiber.Map{
+			"username": "budi",
+			"name":     "Budi",
+		})
+	})
+
+	request := httptest.NewRequest("GET", "/user", nil)
+	request.Header.Set("Accept", "application/json")
+	response, err := app.Test(request)
+	assert.Nil(t, err)
+	assert.Equal(t, 200, response.StatusCode)
+
+	bytes, err := io.ReadAll(response.Body)
+	assert.Nil(t, err)
+	assert.Equal(t, `{"name":"Budi","username":"budi"}`, string(bytes))
+}
